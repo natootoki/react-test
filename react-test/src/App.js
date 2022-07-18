941bd1b26113b3e83d4bfb435f2cbc21226acd7d
natootoki <ikoan0226@gmail.com>
2022-07-18 20:33:12 +0900

    add input

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <p>Hello, world!</p>
       <button>BUTTON</button>
+      <input onKeyDown={(e)=>{console.log(e.key+" pushed")}} onKeyUp={(e)=>{console.log(e.key+" released")}} />
     </div>
   );
 }
