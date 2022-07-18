b2af344e9e91d3bcc5ba83f0a82ab2d74c601e63
natootoki <ikoan0226@gmail.com>
2022-07-18 23:33:32 +0900

    move cursor

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,9 +1,35 @@
+import styled from "styled-components";
+import { useState } from "react"; 
+
+export let position={x:0, y:0}
+export let speed=4;
+
+const keydown=(e)=>{
+  console.log(e.key);
+  if(e.key==="ArrowRight"){
+    position.x+=speed;
+  }else if(e.key==="ArrowLeft"){
+    position.x-=speed;
+  }else if(e.key==="ArrowUp"){
+    position.y-=speed;
+  }else if(e.key==="ArrowDown"){
+    position.y+=speed;
+  }
+  console.log("position("+position.x+", "+position.y+")")
+}
+
+document.body.addEventListener("keydown", keydown);
+
 function App() {
+  const[hoge,setHoge]=useState([position.x, position.y]);
+  console.log("hoge:"+hoge);
+
   return (
     <div>
       <p>Hello, world!</p>
       <button>BUTTON</button>
-      <input onKeyDown={(e)=>{console.log(e.key)}} />
+      <input onKeyDown={(e)=>{console.log(position);setHoge([position.x, position.y]);}} />
+      <div id="cursor" style={{position:"absolute",top:position.y,left:position.x}}>|</div>
     </div>
   );
 }
--- react-test/src/index.js
+++ react-test/src/index.js
@@ -4,31 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-let position={x:0, y:0}
-let speed=4;
-
-const keydown=(e)=>{
-  console.log(e.key);
-  if(e.key==="ArrowRight"){
-    position.x+=speed;
-  }else if(e.key==="ArrowLeft"){
-    position.x-=speed;
-  }else if(e.key==="ArrowUp"){
-    position.y-=speed;
-  }else if(e.key==="ArrowDown"){
-    position.y+=speed;
-  }
-  console.log("position("+position.x+", "+position.y+")")
-}
-
-document.body.addEventListener("keydown", keydown);
+import { position } from ".";
+import { speed } from ".";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
