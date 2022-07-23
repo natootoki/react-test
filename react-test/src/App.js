490dcb599cf98f90bab722d82720d2750a523195
natootoki <ikoan0226@gmail.com>
2022-07-23 23:40:09 +0900

    fix moving

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -2,29 +2,55 @@ import { useEffect, useState } from "react";
 
 export let position={x:0, y:0}
 export let speed=4;
-export let pushed=false;
+export let pushed={right:false, left:false, up:false, down:false};
 
 const keydown=(e)=>{
   console.log(e.key);
   if(e.key==="ArrowRight"){
-    position.x+=speed;
+    pushed.right=true;
   }else if(e.key==="ArrowLeft"){
-    position.x-=speed;
+    pushed.left=true;
   }else if(e.key==="ArrowUp"){
-    position.y-=speed;
+    pushed.up=true;
   }else if(e.key==="ArrowDown"){
-    position.y+=speed;
+    pushed.down=true;
   }
-  console.log("position("+position.x+", "+position.y+")")
+  //console.log("position("+position.x+", "+position.y+")")
+}
+
+const keyup=(e)=>{
+  console.log(e.key);
+  if(e.key==="ArrowRight"){
+    pushed.right=false;
+  }else if(e.key==="ArrowLeft"){
+    pushed.left=false;
+  }else if(e.key==="ArrowUp"){
+    pushed.up=false;
+  }else if(e.key==="ArrowDown"){
+    pushed.down=false;
+  }
 }
 
 document.body.addEventListener("keydown", keydown);
+document.body.addEventListener("keyup", keyup);
 
 function App() {
   const[hoge,setHoge]=useState([position.x, position.y]);
 
   useEffect(()=>{
     const interval = setInterval(()=>{
+      if(pushed.right){
+        position.x+=speed;
+      }
+      if(pushed.left){
+        position.x-=speed;
+      }
+      if(pushed.up){
+        position.y-=speed;
+      }
+      if(pushed.down){
+        position.y+=speed;
+      }
       setHoge([position.x, position.y]);
     },50);
     return ()=>clearInterval(interval);
